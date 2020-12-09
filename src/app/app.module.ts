1af04aa2cfefa46f0ad921da9d980d66926166e4
helenkah90 <helenkahl88@gmail.com>
2020-12-10 00:56:08 +0200

    added basic components, services and models to a newly created project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoTableComponent } from './components/todo-table/todo-table.component';
+import { EditComponent } from './components/edit/edit.component';
+import { LoginComponent } from './components/login/login.component';
+import { ApiComponent } from './shared/services/api/api.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoTableComponent,
+    EditComponent,
+    LoginComponent,
+    ApiComponent
   ],
   imports: [
     BrowserModule,
